64025f34c9511c6d57bab10ab36f3ab4708c9eb6
sena <senika_@naver.com>
2023-02-13 20:27:57 +0900

    #2 page routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+import Main from "./pages/Main";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project/:id" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
