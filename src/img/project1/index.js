6707fb1b8275eda8d9164fa2541e410ebae9f9d0
sena <senika_@naver.com>
2023-02-15 00:47:45 +0900

    #4 project/1

---
+++ src/img/project1/index.js
@@ -0,0 +1,10 @@
+import home from "./home.mp4";
+import main from "./main.mov";
+import user from "./user.mov";
+import user2 from "./user2.mov";
+import board from "./board.mov";
+import board2 from "./board2.mov";
+import board3 from "./board3.mov";
+import board4 from "./board4.mov";
+
+export { home, main, user, user2, board, board2, board3, board4 };
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -2,6 +2,88 @@ import styled from "styled-components";
 import { useParams } from "react-router-dom";
 
 import Nav from "../components/Nav";
+import { secondProjectData } from "../data/second_project_data";
+
+const ArticleContainer = styled.div`
+  width: 50%;
+  float: left;
+  box-sizing: border-box;
+  padding: 12px;
+`;
+
+const Inner = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+  padding: 14px;
+  cursor: pointer;
+  filter: saturate(40%);
+
+  :hover {
+    filter: saturate(120%);
+  }
+
+  video {
+    width: 100%;
+    box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.1);
+  }
+
+  h2 {
+    font: bold 20px/1 "IM_Hyemin-Bold";
+    color: #444;
+    margin-top: 15px;
+    margin-bottom: 15px;
+    text-align: center;
+  }
+
+  p {
+    font: 13px "IM_Hyemin-Bold";
+    color: #777;
+    margin-bottom: 10px;
+    text-align: center;
+    padding: 0 20px;
+  }
+`;
+const Article = (props) => {
+  return (
+    <ArticleContainer>
+      <Inner>
+        <video
+          src={props.video}
+          muted
+          onMouseOver={(e) => e.target.play()}
+          onMouseOut={(e) => e.target.pause()}
+        />
+        <h2>{props.title}</h2>
+        <p>{props.con}</p>
+      </Inner>
+    </ArticleContainer>
+  );
+};
+
+const Section = styled.section`
+  width: 100%;
+`;
+
+const ProjectList = () => {
+  const { id } = useParams();
+
+  return (
+    <Section>
+      {secondProjectData.map((data, idx) => (
+        <Article
+          key={idx}
+          video={data.video}
+          title={data.title}
+          con={data.con}
+        />
+      ))}
+    </Section>
+  );
+};
 
 const Container = styled.div`
   display: flex;
@@ -11,12 +93,10 @@ const Container = styled.div`
 `;
 
 const Project = () => {
-  const { id } = useParams();
-
   return (
     <Container>
       <Nav />
-      <h1>123</h1>
+      <ProjectList />
     </Container>
   );
 };
