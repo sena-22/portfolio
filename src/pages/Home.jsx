d98e1d095c16239a822e89c87606fc95afd8dd09
sena <senika_@naver.com>
2023-02-14 14:29:47 +0900

    #3 third,fouth card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,46 +2,48 @@ import styled from "styled-components";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faBlog } from "@fortawesome/free-solid-svg-icons";
+import { faBlog, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-import { faReact } from "@fortawesome/free-brands-svg-icons"
+import { faReact } from "@fortawesome/free-brands-svg-icons";
 import { faSquareJs } from "@fortawesome/free-brands-svg-icons";
 import { faCss3 } from "@fortawesome/free-brands-svg-icons";
 import { faHtml5 } from "@fortawesome/free-brands-svg-icons";
 
-import moon from '../img/moon.png';
+import moon from "../img/moon.png";
+import stackOverflow from "../img/stack-overflow.png";
+import cuteBird from "../img/cute_bird.png";
 
 const Main = styled.main`
   width: 100%;
   height: 100%;
   perspective: 1300px;
   background-color: #1d1d1d;
-  position:fixed;
-`
+  position: fixed;
+`;
 const Section = styled.section`
-  width:600px;
-  height:800px;
-  position:absolute;
-  top:50%;
-  left:50%;
+  width: 600px;
+  height: 800px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
   margin-top: -400px;
   margin-left: -300px;
-`
+`;
 const Article = styled.article`
-  width:100%;
-  height:100%;
-  position:absolute;
-  top:0px;
-  left:0px;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0px;
+  left: 0px;
 
   h1 {
-    position:absolute;
-    left:0px;
-    top:0px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
     font-size: 35px;
     transition: all 0.5s;
     opacity: 0;
-    font-family: 'IM_Hyemin-Bold';
+    font-family: "IM_Hyemin-Bold";
 
     &::after {
       width: 100%;
@@ -58,17 +60,17 @@ const Article = styled.article`
     }
   }
 
-  :hover h1{
-    top:-75px;
+  :hover h1 {
+    top: -75px;
     opacity: 1;
-    text-shadow: 3px 3px 15px  #BB88ED;
+    text-shadow: 3px 3px 15px #bb88ed;
 
     &::after {
-      background: #BB88ED;
-      box-shadow: 3px 3px 15px  #BB88ED;
+      background: #bb88ed;
+      box-shadow: 3px 3px 15px #bb88ed;
       transform: translate(0, 0);
     }
-}
+  }
 
   .inner {
     width: 100%;
@@ -77,60 +79,59 @@ const Article = styled.article`
 
   .inner > div {
     width: 100%;
-    height:100%;
-    border:1px solid rgba(255,255,255,0.5);
+    height: 100%;
+    border: 1px solid rgba(255, 255, 255, 0.5);
     box-sizing: border-box;
-    background:rgba(255,255,255,0.06);
+    background: rgba(255, 255, 255, 0.06);
     padding: 50px;
     position: relative;
     opacity: 0.8;
     overflow: hidden;
     cursor: pointer;
-    transition:all 0.5s;
+    transition: all 0.5s;
 
     :hover {
-      background: rgba(255,255,255,0.15);
+      background: rgba(255, 255, 255, 0.15);
       transform: scale(1.03);
       opacity: 1;
     }
-
   }
 
   /* box1 */
-   &.box1 .inner > div .inner__meta {
-      align-items: center;
-      display: flex;
-      flex-direction: column;
-   }
+  &.box1 .inner > div .inner__meta {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+  }
 
   &.box1 .inner > div img {
-    width:300px;
-    height:300px;
+    width: 300px;
+    height: 300px;
     margin-top: 20px;
     margin-bottom: 50px;
   }
-  
+
   &.box1 .inner > div h2 {
-    font-size:40px;
+    font-size: 40px;
     margin-bottom: 10px;
-    font-family: 'IM_Hyemin-Bold';
+    font-family: "IM_Hyemin-Bold";
   }
 
   &.box1 .inner > div .inner__meta p {
-    font-size:35px;
+    font-size: 35px;
     margin-bottom: 50px;
-    font-family: 'Poor Story',cursive;
+    font-family: "Poor Story", cursive;
   }
 
-   &.box1 .inner:hover > div .inner__meta h2,
-   &.box1 .inner:hover > div .inner__meta p {
-    text-shadow:  0 0 10px  #BB88ED;
-   }
+  &.box1 .inner:hover > div .inner__meta h2,
+  &.box1 .inner:hover > div .inner__meta p {
+    text-shadow: 0 0 10px #bb88ed;
+  }
 
-  &.box1 .inner > div .inner__link  {
-    position:absolute;
-    top:4%;
-    left:75%;
+  &.box1 .inner > div .inner__link {
+    position: absolute;
+    top: 4%;
+    left: 75%;
   }
 
   &.box1 .inner > div .inner__link .inner__link__icon {
@@ -140,66 +141,267 @@ const Article = styled.article`
     transition: all 0.5s;
 
     &:hover {
-      font-size:33px;
+      font-size: 33px;
     }
   }
 
   /* box2 */
 
   &.box2 .inner > div {
-    width:100%;
-    height:185px;
+    width: 100%;
+    height: 185px;
     margin-bottom: 20px;
 
-    &:hover h2, &:hover span {
-      text-shadow: 5px 0px 20px #BB88ED;
-      transition:all 1.5s;
-      color:#BB88ED;
+    &:hover h2,
+    &:hover span {
+      text-shadow: 5px 0px 20px #bb88ed;
+      transition: all 1.5s;
+      color: #bb88ed;
     }
   }
 
-   &.box2 .inner > div span > * {
-    width:15%;
-    height:100%;
+  &.box2 .inner > div span > * {
+    width: 15%;
+    height: 100%;
     border-right: 1px solid #bbb;
     float: left;
-    color:#fff;
+    color: #fff;
     position: relative;
     box-sizing: border-box;
     padding-right: 25px;
-   }
+  }
 
-   &.box2 .inner > div .con {
-      width:85%;
-      height:100%;
-      box-sizing:border-box;
-      padding-left: 40px;
-      float: right;
-      font-family: 'IM_Hyemin-Bold';
-   }
+  &.box2 .inner > div .con {
+    width: 85%;
+    height: 100%;
+    box-sizing: border-box;
+    padding-left: 40px;
+    float: right;
+    font-family: "IM_Hyemin-Bold";
+  }
 
-     &.box2 .inner > div .con p{
-     font-size:20px;
-     font-family: 'IM_Hyemin-Bold';
-     color:#999;
-     back
-    }
+  &.box2 .inner > div .con p {
+    font-size: 20px;
+    font-family: "IM_Hyemin-Bold";
+    color: #999;
+  }
 
+  /* box3 */
 
+  &.box3 .inner > div {
+    width: 100%;
+    height: 253px;
+    margin-bottom: 20px;
+  }
 
-  `
+  &.box3 .inner > div .pic {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    float: left;
+    position: relative;
+    top: 15px;
+  }
+
+  &.box3 .inner > div:nth-of-type(2) .pic {
+    width: 120px;
+    height: 120px;
+    margin-right: -10px;
+  }
+
+  &.box3 .inner > div .con {
+    width: 340px;
+    float: right;
+  }
+
+  &.box3 .inner > div .con h2 {
+    color: #fff;
+    font-family: "IM_Hyemin-Bold";
+  }
+
+  &.box3 .inner > div:hover .con h2 {
+    text-shadow: 0 0 10px #bb88ed;
+  }
+
+  &.box3 .inner > div .con p {
+    font: 21px "poor story";
+    color: #ddd;
+    margin-top: 20px;
+    overflow-wrap: break-word;
+  }
+
+  &.box3 .inner > div .con span {
+    position: absolute;
+    bottom: 50px;
+    right: 50px;
+    font: 20px "poor story";
+    color: #bb88ed;
+  }
+
+  /* box 4*/
+
+  &.box4 .inner > div:nth-of-type(1) {
+    width: 100%;
+    height: 400px;
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &.box4 .inner > div:nth-of-type(1) .icon {
+    color: #fff;
+    font-size: 200px;
+    opacity: 0.9;
+    margin-bottom: 30px;
+  }
+
+  &.box4 .inner > div:nth-of-type(1) h2 {
+    font-weight: bold;
+    font-size: 40px;
+    font-family: "IM_Hyemin-Bold";
+  }
+
+  &.box4 .inner > div:nth-of-type(2) {
+    font: bold normal 30px "IM_Hyemin-Bold";
+    width: 290px;
+    height: 380px;
+    margin-right: 20px;
+    float: left;
+    text-align: center;
+  }
+
+  &.box4 .inner > div:nth-of-type(2) .icon {
+    font-size: 120px;
+    color: #fff;
+    opacity: 0.8;
+    margin-top: 20px;
+    margin-bottom: 30px;
+  }
+
+  &.box4 .inner > div:nth-of-type(2) h2 {
+    margin-top: 30px;
+    margin-bottom: 14px;
+    font: bold 40px "IM_Hyemin-Bold";
+  }
+
+  &.box4 .inner > div:nth-of-type(3) {
+    width: 290px;
+    height: 380px;
+    text-align: center;
+    font: bold normal 30px "IM_Hyemin-Bold";
+  }
+
+  &.box4 .inner > div:nth-of-type(3) .icon {
+    font-size: 120px;
+    color: #fff;
+    opacity: 0.8;
+    margin-top: 20px;
+    margin-bottom: 30px;
+  }
+  &.box4 .inner > div:nth-of-type(3) h2 {
+    margin-top: 30px;
+    margin-bottom: 14px;
+    font: bold 40px "IM_Hyemin-Bold";
+  }
+
+  &.box4 .inner > div:hover h2 {
+    text-shadow: 5px 0 10px #bb88ed;
+  }
+`;
 
 const Home = () => {
+  const Box2 = (props) => {
+    return (
+      <div>
+        <span>
+          <FontAwesomeIcon icon={props.icon} />
+        </span>
+        <div className="con">
+          <h2>{props.title}</h2>
+          <p>{props.con}</p>
+        </div>
+      </div>
+    );
+  };
+
+  const box2contents = [
+    {
+      icon: faReact,
+      title: "React",
+      con: "React 라이브러리를 이용한 개인 프로젝트 및 팀 프로젝트 진행",
+    },
+    {
+      icon: faSquareJs,
+      title: "Javascript",
+      con: "ES6+ 기반으로 학습",
+    },
+    {
+      icon: faHtml5,
+      title: "HTML",
+      con: "HTML의 기본 문법 학습 및 활용 경험",
+    },
+    {
+      icon: faCss3,
+      title: "CSS",
+      con: "CSS 및 styled-components를 이용한 스타일 적용 경험",
+    },
+  ];
+
+  const box3contents = [
+    {
+      src: moon,
+      title: "Front-line Developer",
+      con: "미니 프로젝트로 개발한 포트폴리오 페이지입니다.",
+      date: "2023-02 (1인/1주)",
+    },
+    {
+      src: cuteBird,
+      title: "Treedom",
+      con: "여행 일정 기록 & 공유 서비스입니다.",
+      date: "2023-1 ~ 2023-2 (7인/4주)",
+    },
+    {
+      src: stackOverflow,
+      title: "Stack Overflow",
+      con: "Stack Overflow 클론 코딩 프로젝트입니다.",
+      date: "2023-02 (6인/2주)",
+    },
+  ];
+
+  const Box3 = (props) => {
+    return (
+      <div>
+        <img className="pic" src={props.src} />
+        <div className="con">
+          <h2>{props.title}</h2>
+          <p>{props.con}</p>
+          <span>{props.date}</span>
+        </div>
+      </div>
+    );
+  };
+
+  const Box4 = (props) => {
+    return (
+      <div>
+        <FontAwesomeIcon className="icon" icon={props.icon} />
+        <h2>{props.title}</h2>
+      </div>
+    );
+  };
+
   return (
     <>
       <Main>
         <Section>
-          {/* <Article className="box1">
+          <Article className="box1">
             <h1>Profile</h1>
             <div className="inner">
               <div>
                 <div className="inner__meta">
-                  <img src={moon} alt="profile_image"/>
+                  <img src={moon} alt="profile_image" />
                   <h2>김현정</h2>
                   <p>Frontend Developer</p>
                 </div>
@@ -210,67 +412,42 @@ const Home = () => {
                   <span className="inner__link__icon">
                     <FontAwesomeIcon icon={faBlog} />
                   </span>
-              </div>
+                </div>
               </div>
             </div>
-          </Article> */}
+          </Article>
           <Article className="box2">
             <h1>Stack</h1>
             <div className="inner">
-              <div>
-                <span>
-                  <FontAwesomeIcon icon={faReact}/>
-                </span>
-                <div className="con">
-                  <h2>React</h2>
-                  <p>React 라이브러리를 이용한 개인 프로젝트 및 팀 프로젝트 진행</p>
-                </div>
-              </div>
-                  <div>
-                <span>
-                  <FontAwesomeIcon icon={faSquareJs}/>
-                </span>
-                <div className="con">
-                  <h2>Javascript</h2>
-                  <p>ES6+ 기반으로 학습</p>
-                </div>
-              </div>
-                  <div>
-                <span>
-                  <FontAwesomeIcon icon={faHtml5}/>
-                </span>
-                <div className="con">
-                  <h2>HTML</h2>
-                  <p>HTML의 기본 문법 학습 및 활용 경험</p>
-                </div>
-              </div>
-               <div>
-                <span>
-                  <FontAwesomeIcon icon={faCss3}/>
-                </span>
-                <div className="con">
-                  <h2>CSS</h2>
-                  <p>CSS 및 styled-components를 이용한</p>
-                  <p> 스타일 적용 경험</p>
-                </div>
-              </div>
+              {box2contents.map((con) => (
+                <Box2 icon={con.icon} title={con.title} con={con.con} />
+              ))}
             </div>
           </Article>
-          {/* <Article claclassNamess="box3">
+          <Article className="box3">
             <h1>Project</h1>
             <div className="inner">
-              <div></div>
+              {box3contents.map((con, idx) => (
+                <Box3
+                  key={idx}
+                  src={con.src}
+                  title={con.title}
+                  con={con.con}
+                  date={con.date}
+                />
+              ))}
             </div>
           </Article>
-          <Article claclassNamess="box4">     
+          <Article className="box4">
             <h1>Contact</h1>
             <div className="inner">
-              <div></div>
+              <Box4 icon={faEnvelope} title={"Contact Me"} />
+              <Box4 icon={faGithub} title={"Github"} />
+              <Box4 icon={faBlog} title={"Blog"} />
             </div>
-          </Article> */}
+          </Article>
         </Section>
       </Main>
-
     </>
   );
 };
