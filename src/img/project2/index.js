ea8ffeb86e87e22b1e0f89bd3cbce56cbd5f285a
sena <senika_@naver.com>
2023-02-16 00:21:18 +0900

    #4 video file 변경

--- src/img/project2/index.js
+++ src/img/project2/index.js
@@ -1,10 +1,10 @@
 import home from "./home.mp4";
-import main from "./main.mov";
+import main from "./main.mp4";
 import user from "./user.mov";
 import user2 from "./user2.mov";
 import board from "./board.mov";
-import board2 from "./board2.mov";
-import board3 from "./board3.mov";
-import board4 from "./board4.mov";
+import board2 from "./board2.mp4";
+import board3 from "./board3.mp4";
+import board4 from "./board4.mp4";
 
 export { home, main, user, user2, board, board2, board3, board4 };
