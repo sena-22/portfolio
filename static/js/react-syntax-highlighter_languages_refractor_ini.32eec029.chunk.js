50ae402c11cdb1a498d7bffb222d93df2f0e443d
sena <senika_@naver.com>
2023-02-17 02:10:56 +0900

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_ini.32eec029.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[6495],{3409:function(t){function n(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=n,n.displayName="ini",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.32eec029.chunk.js.map
